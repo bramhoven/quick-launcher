f03595bf45d8701fa694a13985239be88d6db58f
Bram Hoven <bram.hoven@weareyou.com>
2019-11-11 16:07:28 +0100

    Add volume controls

--- renderer.js
+++ renderer.js
@@ -6,6 +6,26 @@
 // process.
 const mediaController = require('node-media-controller');
 const {ipcRenderer, shell} = require('electron');
+const audio = require('win-audio').speaker;
+audio.polling(200);
+
+audio.events.on('change', (volume) => {
+  document.getElementById('volume-range').value = volume.new;
+});
+
+const setVolume = (event) => {
+  audio.set(parseInt(event.target.value));
+};
+
+const volumeUp = (event) => {
+  let volume = audio.get();
+  audio.set(volume + 2 < 100 ? volume + 2 - (volume%2) : 100);
+};
+
+const volumeDown = (event) => {
+  let volume = audio.get();
+  audio.set(volume - 2 > 0 ? volume - 2 + (volume%2) : 0);
+};
 
 const prevMedia = (event) => {
   mediaController.executeCommand('previous', () => {});
@@ -31,17 +51,22 @@ const closeQuickLauncher = (event) => {
 
 const hotkeyManager = (event) => {
   let keyCode = event.keyCode;
-  if(keyCode == 'b'.charCodeAt(0)) prevMedia();
-  if(keyCode == 'p'.charCodeAt(0)) togglePlayMedia();
-  if(keyCode == 'n'.charCodeAt(0)) nextMedia();
-  if(keyCode == 'c'.charCodeAt(0)) openChrome();
-  if(keyCode == 'q'.charCodeAt(0)) closeQuickLauncher();
+  if(keyCode == 38) volumeUp(); // Up arrow
+  if(keyCode == 40) volumeDown(); // Down arrow
+  if(keyCode == 'B'.charCodeAt(0)) prevMedia();
+  if(keyCode == 'P'.charCodeAt(0)) togglePlayMedia();
+  if(keyCode == 'N'.charCodeAt(0)) nextMedia();
+  if(keyCode == 'C'.charCodeAt(0)) openChrome();
+  if(keyCode == 'Q'.charCodeAt(0)) closeQuickLauncher();
 };
+
+document.getElementById('volume-range').value = audio.get();
 
 document.getElementById('prev').addEventListener('click', prevMedia);
 document.getElementById('play-pause').addEventListener('click', togglePlayMedia);
 document.getElementById('next').addEventListener('click', nextMedia);
 document.getElementById('chrome').addEventListener('click', openChrome);
 document.getElementById('ql-close').addEventListener('click', closeQuickLauncher);
+document.getElementById('volume-range').addEventListener('input', setVolume)
 
-document.addEventListener('keypress', hotkeyManager);
+document.addEventListener('keydown', hotkeyManager);
